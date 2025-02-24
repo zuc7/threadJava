91ac59cde431f4f2a0e00313385c1d1a4e6ec733
4B15 <4B15@LABICA3.local>
2025-02-24 08:54:14 +0100

    update, class già esistente

--- Esempio2.java
+++ Esempio2.java
@@ -1,15 +1,5 @@
 package trd;
 
-class Saluti extends Thread{
-	public Saluti(String nome) {
-		super(nome);
-	}
-	public void run() {
-		for(int i=1; i <=10; i++) {
-			System.out.println("Ciao da "+ getName()+ "   "+i);
-		}
-	}	
-}
 public class Esempio2{
 	public static void main(String[] args) throws InterruptedException {
 		Saluti s= new Saluti("n di n");
